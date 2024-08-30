bdda47864873a668747249407f96a2c5c4d3ba87
shifana mattil <shifanamattil@gmail.com>
2024-08-30 20:09:26 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import {Tictactoe } from './Components/TicTacToe/Tictactoe';
+
+function App() {
+  return (
+    <div>
+      <Tictactoe/>
+      
+    </div>
+  );
+}
+
+export default App;
